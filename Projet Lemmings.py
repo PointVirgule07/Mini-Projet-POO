e93919fdfca797f12574d42f7a8f105121e33f27
PointVirgule07 <virgile.thabouret@robolyon.com>
2024-10-01 19:27:17 +0200

    NSM

--- Projet Lemmings.py
+++ Projet Lemmings.py
@@ -1,1 +1,24 @@
-#on vas s'amuser
+class jeu():
+    def __init__(self):
+        self.__grotte = []
+
+    def affiche(self):
+        for i in range(len(self.__grotte)):
+            print(self.__grotte[i])
+    
+    def tour(self):
+        pass #chai pas quoi faire encore
+
+    def demarre(self):
+        while True:
+            action = input("l pour ajouter un lemming, q pour quitter, juste entrer pour continuer")
+            
+            if action == "l":
+                #ajouter un lemming
+                pass
+
+            elif action == "q":
+                break 
+
+            self.tour()
+            self.affiche()
